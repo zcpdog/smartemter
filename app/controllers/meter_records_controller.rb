14cf69e8cab784316c5a4d0a8e87101d50ebc94c
Chuanpin Zhu <zcppku@gmail.com>
2014-02-10 13:07:03 -0800

    v1

--- app/controllers/meter_records_controller.rb
+++ app/controllers/meter_records_controller.rb
@@ -9,13 +9,14 @@ class MeterRecordsController < ApplicationController
   
   def monitor
     @meter = Meter.find(params[:meter_id])
-    @meter_record = MeterRecord.unread.produced_by(@meter).order("RAND()").last
+    @meter_record = MeterRecord.produced_by(@meter).last
+    @emter_record.is_read = true;
     render json: @meter_record
   end
   
   def summary
     @meter = Meter.find(params[:meter_id])
-    @meter_records = MeterRecord.produced_by(@meter).between(params[:begin_at],params[:end_at]);
+    @meter_records = MeterRecord.produced_by(@meter);
     respond_to do |format|
       format.html
     end
--- app/helpers/meter_records_helper.rb
+++ app/helpers/meter_records_helper.rb
@@ -1,6 +1,48 @@
 module MeterRecordsHelper
   def init_data type
     MeterRecord.recent.map{|mr|"[#{mr.created_at.strftime('%s')},#{eval("mr."+type)}]"}.join(",")
-    ""
   end
+  
+  def show_summary(bt,et)
+    current = 0
+    flage = false
+    data = Array.new
+    MeterRecord.between(bt,et).each do |mr|
+      if(flage) 
+        current+=mr.Energy_kWH
+        data.push current
+      else
+        data.push mr.Energy_kWH
+      end
+    end
+    "[#{data.join(',')}]"
+  end
+  
+  def summary_time
+    MeterRecord.first.created_at || Time.new
+  end
+  
+  def show_summary_temp(mrs)
+    current = 0
+    flage = false
+    data = Array.new
+    max = 0
+    low = 0;
+    mrs.each do |mr|
+      if mr.Energy_kWH>max
+        max = mr.Energy_kWH
+      else
+        low = mr.Energy_kWH
+        flage = true
+      end
+      if flage
+        current+=(mr.Energy_kWH-low)
+        data.push current
+      else
+        data.push mr.Energy_kWH
+      end
+    end
+    "[#{data.join(',')}]"
+  end
+  
 end
