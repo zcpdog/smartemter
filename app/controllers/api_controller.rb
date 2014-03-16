bcfb3047d785afa66d5e3ceefe4cb5ecd14329f2
zcpdog <zcppku@gmail.com>
2014-03-15 23:49:45 -0700

    add parameters inspect

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,6 +4,9 @@ class ApiController < ApplicationController
   end
   
   def upload
+    puts "==================================="
+    puts params.inspect
+    puts "==================================="
     uploaded_io = params[:filePath]
     File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
       file.write(uploaded_io.read)
